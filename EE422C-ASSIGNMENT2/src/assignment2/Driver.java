b4c2f3eb9ce0ea0006a006891d7408b9fe001613
mina abbassian <mina.abbassian@gmail.com>
2020-09-17 11:19:55 -0500

    starter code

---
+++ EE422C-ASSIGNMENT2/src/assignment2/Driver.java
@@ -0,0 +1,23 @@
+/*
+ * EE422C Project 2 (Mastermind) submission by
+ * Replace <...> with your actual data. 
+ * <Student Name>
+ * <Student EID>
+ * Slip days used: <0>
+ * Summer 2020
+ */
+
+package assignment2;
+
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) {
+        // Use this for your testing.  We will not be calling this method.
+    }
+
+    public static void start(Boolean isTesting, GameConfiguration config, SecretCodeGenerator generator) {
+        // TODO: complete this method
+		// We will call this method from our JUnit test cases.
+    }
+}
